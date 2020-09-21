03be15ff79ac49698a63791e13b49d6b5da64847
Habib Afolabi <habibwritescode@gmail.com>
2020-09-21 22:11:55 +0100

    Write frontend code to handle stripe payment request

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51HQJxUHvo9nRbsnAVx3OEpKQeu95qG0yWsasewznm9M1DRUEI77by3R1xN9LjjLoAmW65q8pty14w0BxmUxB0f8300AEYTeRk1';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment successful')
-    }
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        })
+            .then(response => {
+                alert('Payment successful');
+            })
+            .catch(error => {
+                console.log('Payment error: ', JSON.parse(error));
+                alert('There was an issue with your payment. Please make sure you are using the provided credit card.');
+            });
+    };
 
     return (
         <StripeCheckout
